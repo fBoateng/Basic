0783d9ef402a6ea2640ce9094f07450de77aff3d
fboateng <b0af@ymail.com>
2023-11-10 16:11:33 +0000

    whew

--- main_menu2.py
+++ main_menu2.py
@@ -1,5 +1,4 @@
-import os
-import json
+import os, json
 
 def main_menu(orders):
     while True:
@@ -8,24 +7,25 @@ def main_menu(orders):
             print('You entered an empty order. exiting.')
             return
         print('Check your order:')
-        print(order)
+        print_order(order)
         confirm = input('Confirm? Press Y to confirm, N to cancel: ').capitalize
         if confirm == 'Y':
-            save_order(orders, 'orders.json')
+            orders.append(order)
             print('Thanks for your order!')
             print_order(order)
         else:
             continue
         
-
-def menu(choices, title, prompt):
+        
+        
+def menu(choices, title = 'Erik\'s Menu', prompt= 'Choose your item: '):
     print(title)
-    print('-' *  len(title))
+    print('-' + '+' *  len(title))
     i = 1
     for c in choices:
         print(i, c)
         i += 1
-    while True:    
+    while True:
         choice = input(prompt)
         allowed_answers = []
         for a in range(1, len(choices) + 1):
@@ -36,17 +36,14 @@ def menu(choices, title, prompt):
             if choice == 'X' or choice == 'x':
                 answer = ''
                 break
-                
             else:
                 answer = choices[int(choice) - 1]
-        
                 break
         else:
-            print('Enter a number from 1 to', len(choices))
+            print('Enter number from 1 to ', len(choices))
             answer = ''
-        
-    
     return answer
+
 
 
 def read_menu(filename):
@@ -54,23 +51,23 @@ def read_menu(filename):
     temp = f.readlines()
     result = []
     for item in temp:
-        result.append(item.strip())
-    f.close()
+        new_item = item.strip()
+        result.append(new_item)
     return result
-
 
 def get_order():
     order = {}
-    order['name'] = input('What is your name: ').capitalize()
+    name = input("Enter your name or enter 'X' to exit: ")
+    if name == 'x'.capitalize() or name == 'X':
+        return {}
+    else:
+        order["name"] = name
     drinks = read_menu('drinks.txt')
-    flavor = read_menu('flavor.txt')
+    flavors = read_menu('flavors.txt')
     toppings = read_menu('toppings.txt')
-    drink = menu(drinks, 'What drink would you like?', 'Enter a number: ')
-    flavor = menu(flavor, 'What flavor would you like?', 'Enter a number: ')
-    toppings = menu(toppings, 'What toppings would you like?', 'Enter a number: ')
-    order['drink'] = drink
-    order['flavor'] = flavor
-    order['toppings'] = toppings
+    order["drink"] = menu(drinks, 'Erik\'s drinks', 'Choose your drink: ')
+    order["flavor"] = menu(flavors, 'Erik\'s flavors', 'Choose your flavor: ')
+    order["topping"] = menu(toppings, 'Erik\'s toppings', 'Choose your topping: ')
     return order
 
 
@@ -78,9 +75,10 @@ def print_order(order):
     print("Here is your order, ", order["name"])
     print("Main product: ", order["drink"])
     print("Flavor: ", order["flavor"])
-    print("Topping: ", order["toppings"])
+    print("Topping: ", order["topping"])
     print("Thanks for your order!")
     return
+
 
 def save_order(orders, filename):
     f = open(filename, 'w')
@@ -96,7 +94,14 @@ def load_orders(filename):
     else:
         orders = []
         return orders
-
+    
+    
 orders = load_orders('orders.json')
 main_menu(orders)
 save_order(orders, 'orders.json')
+
+
+
+
+    
+      
