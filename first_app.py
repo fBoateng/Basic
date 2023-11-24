3e79554714c2760557ce8b22b33f4add1d49f2cb
fboateng <b0af@ymail.com>
2023-11-24 11:57:08 +0000

    test 1

--- first_app.py
+++ first_app.py
@@ -1,18 +1,4 @@
-from flask import Flask, render_template, request
-
-app = Flask(__name__)
-
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-
-@app.route('/hello/<name>')
-def greet(name='Stranger'):
-    return render_template('greeting.html', name= name)
-
-
+# c:/Users/se/Desktop/Basic/first_app.py
 
 import json
 import os
@@ -61,6 +47,41 @@ def order():
         return render_template(
             "print.html", new_order=new_order
         )
+        
+        
+        
+def read_menu(filename):
+    f = open(filename)
+    temp = f.readlines()
+    result = []
+    for item in temp:
+        new_item = item.strip()
+        result.append(new_item)
+    return result
+
+def get_order():
+    order = {}
+    name = input("Enter your name or enter 'X' to exit: ")
+    if name == 'x'.capitalize() or name == 'X':
+        return {}
+    else:
+        order["name"] = name
+    drinks = read_menu('drinks.txt')
+    flavors = read_menu('flavors.txt')
+    toppings = read_menu('toppings.txt')
+    order["drink"] = menu(drinks, 'Erik\'s drinks', 'Choose your drink: ')
+    order["flavor"] = menu(flavors, 'Erik\'s flavors', 'Choose your flavor: ')
+    order["topping"] = menu(toppings, 'Erik\'s toppings', 'Choose your topping: ')
+    return order
+
+
+def print_order(order):
+    print("Here is your order, ", order["name"])
+    print("Main product: ", order["drink"])
+    print("Flavor: ", order["flavor"])
+    print("Topping: ", order["topping"])
+    print("Thanks for your order!")
+    return
 
     return render_template("forms.html")
 
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,29 +1,32 @@
-def menu(choices, title, prompt):
-    print(title)
-    print('-' *  len(title))
-    i = 1
-    for c in choices:
-        print(i, c)
-        i += 1
-    while True:    
-        choice = input(prompt)
-        allowed_answers = []
-        for a in range(1, len(choices) + 1):
-            allowed_answers.append(str(a))
-            allowed_answers.append('X')
-            allowed_answers.append('x')
-        if choice in allowed_answers:
-            if choice == 'X' or choice == 'x':
-                answer = ''
-                break
-                
-            else:
-                answer = choices[int(choice) - 1]
-        
-                break
-        else:
-            print('Enter a number from 1 to', len(choices))
-            answer = ''
-        
-    
-    return answer
+def read_menu(filename):
+    f = open(filename)
+    temp = f.readlines()
+    result = []
+    for item in temp:
+        new_item = item.strip()
+        result.append(new_item)
+    return result
+
+def get_order():
+    order = {}
+    name = input("Enter your name or enter 'X' to exit: ")
+    if name == 'x'.capitalize() or name == 'X':
+        return {}
+    else:
+        order["name"] = name
+    drinks = read_menu('drinks.txt')
+    flavors = read_menu('flavors.txt')
+    toppings = read_menu('toppings.txt')
+    order["drink"] = menu(drinks, 'Erik\'s drinks', 'Choose your drink: ')
+    order["flavor"] = menu(flavors, 'Erik\'s flavors', 'Choose your flavor: ')
+    order["topping"] = menu(toppings, 'Erik\'s toppings', 'Choose your topping: ')
+    return order
+
+
+def print_order(order):
+    print("Here is your order, ", order["name"])
+    print("Main product: ", order["drink"])
+    print("Flavor: ", order["flavor"])
+    print("Topping: ", order["topping"])
+    print("Thanks for your order!")
+    return
